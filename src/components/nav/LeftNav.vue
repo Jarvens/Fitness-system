711d02e389deb411e9bdf73f4ee468faa65a3343
Jarven <769558579@qq.com>
2017-07-09 20:50:49 +0800

    新增菜单

--- src/components/nav/LeftNav.vue
+++ src/components/nav/LeftNav.vue
@@ -1,40 +1,51 @@
+
 <template>
     <div>
         <Menu :theme="theme3" active-name="member"  @on-select="click">
-            <Menu-group>
                 <Menu-item name="member">
-                    <Icon type="person" size="30"></Icon>
+                    <Icon type="person" size="20"></Icon>
                     会员
                 </Menu-item>
                 <Menu-item name="private-class">
-                    <Icon type="android-walk" size="30"></Icon>
+                    <Icon type="android-walk" size="20"></Icon>
                     私教
                 </Menu-item>
                 <Menu-item name="group-class">
-                    <Icon type="android-bicycle" size="30"></Icon>
+                    <Icon type="android-bicycle" size="20"></Icon>
                     团体
                 </Menu-item>
                 <Menu-item name="coach">
-                    <Icon type="ios-people" size="30"></Icon>
+                    <Icon type="ios-people" size="20"></Icon>
                     教练
                 </Menu-item>
                 <Menu-item name="vip">
-                    <Icon type="card" size="30"></Icon>
+                    <Icon type="card" size="20"></Icon>
                     VIP
                 </Menu-item>
                 <Menu-item name="stadium">
-                    <Icon type="ios-home" size="30"></Icon>
+                    <Icon type="ios-home" size="20"></Icon>
                     场馆
                 </Menu-item>
                 <Menu-item name="activity">
-                    <Icon type="ios-game-controller-b" size="30"></Icon>
+                    <Icon type="ios-game-controller-b" size="20"></Icon>
                     活动
                 </Menu-item>
                 <Menu-item name="news">
-                    <Icon type="chatbubble-working" size="30"></Icon>
+                    <Icon type="chatbubble-working" size="20"></Icon>
                     新闻
                 </Menu-item>
-            </Menu-group>
+              <Menu-item name="meal">
+                <Icon type="fork" size="20"></Icon>
+                餐饮
+              </Menu-item>
+          <Menu-item name="meal">
+            <Icon type="star" size="20"></Icon>
+            印象
+          </Menu-item>
+          <Menu-item name="statistics">
+            <Icon type="stats-bars" size="20"></Icon>
+            统计
+          </Menu-item>
         </Menu>
     </div>
 
