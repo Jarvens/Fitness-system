3490b609be4cc62a016bdfefc6f80ad2edfdefcf
Jarven <769558579@qq.com>
2017-07-09 20:31:18 +0800

    菜单地址跳转

--- src/components/nav/LeftNav.vue
+++ src/components/nav/LeftNav.vue
@@ -6,15 +6,15 @@
                     <Icon type="person" size="30"></Icon>
                     会员
                 </Menu-item>
-                <Menu-item name="2">
+                <Menu-item name="private-class">
                     <Icon type="android-walk" size="30"></Icon>
                     私教
                 </Menu-item>
-                <Menu-item name="3">
+                <Menu-item name="group-class">
                     <Icon type="android-bicycle" size="30"></Icon>
                     团体
                 </Menu-item>
-                <Menu-item name="4">
+                <Menu-item name="coach">
                     <Icon type="ios-people" size="30"></Icon>
                     教练
                 </Menu-item>
@@ -26,11 +26,11 @@
                     <Icon type="ios-home" size="30"></Icon>
                     场馆
                 </Menu-item>
-                <Menu-item name="7">
+                <Menu-item name="activity">
                     <Icon type="ios-game-controller-b" size="30"></Icon>
                     活动
                 </Menu-item>
-                <Menu-item name="8">
+                <Menu-item name="news">
                     <Icon type="chatbubble-working" size="30"></Icon>
                     新闻
                 </Menu-item>
