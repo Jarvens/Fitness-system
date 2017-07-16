ffddda3e103148e0bef1f0e9d6da609cb17b5ead
Jarven <769558579@qq.com>
2017-07-16 22:37:45 +0800

    会员修改模态

--- src/views/member/Member.vue
+++ src/views/member/Member.vue
@@ -77,6 +77,126 @@
         <Button type="primary" >确定</Button>
       </div>
     </Modal>
+
+    <!--Prompt提示框-->
+    <Modal v-model="open" width="360">
+      <p slot="header" style="color:#f60;text-align:center">
+        <Icon type="information-circled"></Icon>
+        <span>注销确认</span>
+      </p>
+      <div style="text-align:center">
+        <p>是否继续此操作？</p>
+      </div>
+      <div slot="footer">
+        <Button type="error" size="large" long @click="cancelMemberOkHandler()">删除</Button>
+      </div>
+    </Modal>
+
+
+
+    <!--会员修改模态-->
+    <Modal title="会员修改" v-model="modifyModal" width="1000">
+      <Form ref="member" :model="member" :rules="memberValidate" :label-width="80">
+        <Row>
+          <Col span="8">
+          <img src="" alt="头像" style="height: 140px;width: 140px;">
+          </Col>
+          <Col span="8">
+          <Form-item label="姓名" prop="name">
+            <Input v-model="member.name" placeholder="请输入姓名"></Input>
+          </Form-item>
+          </Col>
+          <Col span="8">
+          <Form-item label="手机" prop="phone">
+            <Input v-model="member.phone" placeholder="请输入手机"></Input>
+          </Form-item>
+          </Col>
+          <Col span="8">
+          <Form-item label="生日" prop="bornDate">
+            <Date-picker style="width:250px;" type="date" placeholder="选择日期" v-model="member.bornDate"></Date-picker>
+          </Form-item>
+          </Col>
+          <Col span="8">
+          <Form-item label="联系电话" prop="tel">
+            <Input v-model="member.tel" placeholder="请输入联系电话"></Input>
+          </Form-item>
+          </Col>
+          <Col span="8">
+          <Form-item label="年龄" prop="age">
+            <Input v-model="member.age" placeholder="请输入年龄"></Input>
+          </Form-item>
+          </Col>
+          <Col span="8">
+          <Form-item label="昵称" prop="mail">
+            <Input v-model="member.mail" placeholder="请输入昵称"></Input>
+          </Form-item>
+          </Col>
+          <Col span="6">
+          <Form-item label="状态" prop="status">
+            <Select v-model="member.status" placeholder="请选择状态">
+              <Option value="1">正常</Option>
+              <Option value="2">审核不通过</Option>
+              <Option value="3">注销</Option>
+            </Select>
+          </Form-item>
+          </Col>
+          <Col span="6">
+          <Form-item label="场馆" prop="stadiumId">
+            <Select v-model="member.stadiumId" placeholder="请选择场馆" @on-change="stadiumChangeHandler()">
+              <Option :value="stadium.id" :key="stadium.id" v-for="stadium in stadiumList">{{stadium.name}}</Option>
+            </Select>
+          </Form-item>
+          </Col>
+          <Col span="6">
+          <Form-item label="邮箱" prop="email">
+            <Input v-model="member.email" placeholder="请输入邮箱"></Input>
+          </Form-item>
+          </Col>
+          <Col span="6">
+          <Form-item label="证件号码" prop="identityCard">
+            <Input v-model="member.identityCard" placeholder="请输入证件号码"></Input>
+          </Form-item>
+          </Col>
+        </Row>
+        <Row>
+          <Col span="6">
+          <Form-item label="会员卡" prop="memberCardId">
+            <Select v-model="member.memberCardId" placeholder="请选择会员卡">
+              <Option :value="card.id" :key="card.id" v-for="card in memberCardList">{{card.typeName}}-{{card.money}}</Option>
+            </Select>
+          </Form-item>
+          </Col>
+          <Col span="6">
+          <Form-item label="性别" prop="sex">
+            <Select v-model="member.sex" placeholder="请选择性别">
+              <Option value="1">男</Option>
+              <Option value="0">女</Option>
+            </Select>
+          </Form-item>
+          </Col>
+          <Col span="12">
+          <Form-item label="备注" prop="remark">
+            <Input v-model="member.remark" placeholder="请输入备注"></Input>
+          </Form-item>
+          </Col>
+        </Row>
+        <Row>
+          <Col span="24">
+          <Form-item label="联系地址" prop="address">
+            <Input v-model="member.address" placeholder="请输入联系地址"></Input>
+          </Form-item>
+          </Col>
+        </Row>
+      </Form>
+      <div slot="footer">
+        <Row>
+          <Col span="8" offset="8">
+          <Button type="primary" long @click="submitValidate('memberForm')">确定</Button>
+          </Col>
+        </Row>
+
+      </div>
+    </Modal>
   </div>
 </template>
 
@@ -97,8 +217,13 @@
         member:{},
         memberInfoModal:false,
         ruleValidate:{
+        },
+        open:false,
+        stadiumList:[],
+        memberValidate:{
 
-        }
+        },
+        modifyModal:false
       }
     },
     methods:{
@@ -141,8 +266,35 @@
           }
         })
       },
+      //跳转登录页面
       toRegister(){
         this.$router.push('register')
+      },
+      //注销会员打开prompt
+      cancelHandler(param){
+        this.member=param
+        this.open=true
+      },
+      //确认注销操作
+      cancelMemberOkHandler(){
+        this.$http.post('/member/cancel?id='+this.member.id).then(res=>{
+          if(res.result==1){
+            this.$Message.success('注销会员成功')
+            this.pageListHandler()
+          }else{
+            this.$Message.error('注销会员失败')
+          }
+        })
+      },
+      submitValidate(val){
+
+      },
+      toModifyMember(param){
+        this.member =param.row
+        this.modifyModal=true
+      },
+      stadiumChangeHandler(){
+
       }
     },
     computed: {
@@ -205,8 +357,8 @@
                 },
                 on: {
                   click: () => {
-                    this.memberInfoModal=true
-                    this.show(params.index)
+                    this.modifyModal=true
+                    this.toModifyMember(param)
                   }
                 }
               }, '修改'),
@@ -231,7 +383,7 @@
                 },
                 on: {
                   click: () => {
-                    this.remove(params.index)
+                    this.cancelHandler(param.row)
                   }
                 }
               }, '注销')
